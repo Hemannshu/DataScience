e75f490ef0d87c3c34045515e4a9ce2ac8f93bf8
Himanshu Sharma <141823832+Hemannshu@users.noreply.github.com>
2024-09-10 00:42:06 +0530

    Add files via upload

---
+++ numbers_exercise1.py
@@ -0,0 +1,7 @@
+num_packets=9
+packet_cost=1.49
+total_cost=(num_packets)*(packet_cost)
+print("total cost of shopping = ",total_cost)
+cost_given_by_me=20
+return_cost=cost_given_by_me-total_cost
+print("shopkeeper gives ", return_cost)
